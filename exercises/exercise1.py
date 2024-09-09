facf1562e596fc48ca108c120ebe7940ef940802
Janith Gayashan <janithgayashan9@gmail.com>
2024-09-09 18:52:36 +0530

    new ex

--- exercises/exercise1.py
+++ exercises/exercise1.py
@@ -1,6 +1,21 @@
 # exercises/exercise1.py
 def solve_exercise1():
     print("Solving Exercise 1")
-    name = input("Enter your name")
-    print(name)
-    # Your logic for Exercise 1 goes here
+    name = "sunil"
+    print(name[2])
+    print(name[-1])
+
+    total = 150
+    newTotal = 200
+
+    print(total)
+    print(newTotal)
+    print(total + newTotal)
+
+    bill = 100.75 #this is the floating data type
+    print(bill)
+    
+    bool = True
+    print(bool)
+
+    print(False)
