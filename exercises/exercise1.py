76c79442bd25c2827044f1da2061348d7edb2923
Janith Gayashan <janithgayashan9@gmail.com>
2024-09-12 23:47:43 +0530

    create bmi calculator

--- exercises/exercise1.py
+++ exercises/exercise1.py
@@ -15,7 +15,24 @@ def solve_exercise1():
     bill = 100.75 #this is the floating data type
     print(bill)
     
-    bool = True
-    print(bool)
+    bool_name = True
+    print(bool_name)
 
     print(False)
+    
+    #type checking
+    print(type("This is String datatype")) #type() is inbuild fuction like len function
+    print(type(1234))
+    print(type(350.84))
+    print(type(True))
+
+    # name = input("Enter your name")
+    # length_name = len(name)
+    # print("Number of letters in your name: " + str(length_name))
+
+    #bmi calculator
+    height = 1.65
+    weight = 84
+
+    bmi = weight/(height**2)
+    print(bmi)
