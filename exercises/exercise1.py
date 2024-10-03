44233bcf32c94a16d32a2001541e0d9d61b84186
Janith Gayashan <janithgayashan9@gmail.com>
2024-10-03 16:40:10 +0530

    day 2 final code

--- exercises/exercise1.py
+++ exercises/exercise1.py
@@ -20,19 +20,26 @@ def solve_exercise1():
 
     print(False)
     
-    #type checking
+    # type checking
     print(type("This is String datatype")) #type() is inbuild fuction like len function
     print(type(1234))
     print(type(350.84))
     print(type(True))
 
-    # name = input("Enter your name")
-    # length_name = len(name)
-    # print("Number of letters in your name: " + str(length_name))
+    name = input("Enter your name")
+    length_name = len(name)
+    print("Number of letters in your name: " + str(length_name))
 
-    #bmi calculator
+    # bmi calculator
     height = 1.65
     weight = 84
 
     bmi = weight/(height**2)
     print(bmi)
+
+    print("Welcome to the tip calculator!")
+    total_bill = float(input("What is the total bill?")) #all inputs are get in as string so we want to type conversion into float or int 
+    tip = int(input("How much tip would like to give? 10, 12, or 15?"))
+    split_size = int(input("How many people to split the bil?"))
+    pay_person = (total_bill + total_bill*tip/100)/split_size
+    print(pay_person)
