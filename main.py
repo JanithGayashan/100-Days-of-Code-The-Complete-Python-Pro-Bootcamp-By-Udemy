48fdb977a8e6febd57aedd187548b3c431bbfc0a
Janith Gayashan <janithgayashan9@gmail.com>
2024-10-03 17:15:59 +0530

    day 3 first exercises

--- main.py
+++ main.py
@@ -1,9 +1,11 @@
 from exercises.exercise1 import solve_exercise1
+from exercises.exercise2 import solve_exercise2
 
 def main():
     print("Welcome to python Exercises")
 
-    solve_exercise1()
+    # solve_exercise1()
+    solve_exercise2()
 
 if __name__ == "__main__":
     main()
