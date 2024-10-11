e07d963a21da27aff5b339c46473274c0c4d0e44
Janith Gayashan <janithgayashan9@gmail.com>
2024-10-11 15:36:10 +0530

    Build Treasure Island Game

--- main.py
+++ main.py
@@ -1,11 +1,13 @@
 from exercises.exercise1 import solve_exercise1
 from exercises.exercise2 import solve_exercise2
+from exercises.treasureIsland import treasure_island
 
 def main():
     print("Welcome to python Exercises")
 
     # solve_exercise1()
-    solve_exercise2()
+    # solve_exercise2()
+    treasure_island()
 
 if __name__ == "__main__":
     main()
